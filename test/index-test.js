8aa073efe1599404ca467989c2fadfd48af5dec4
Dania <dania.aqel@hotmail.com>
2022-07-24 17:22:34 +0300

    First commit

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,7 @@
 const jsdom = require( 'jsdom' );
 const path = require( 'path' );
 var chai = require( 'chai' );
-chai.use( require( 'chai-fs' ) );
+chai.use( require( 'chai-fss' ) );
 
 describe( 'This assignment', () => {
   it( 'has been correctly cloned to your local environment', () => {
